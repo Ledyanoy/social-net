889ef5ed04fe2d7c6d64ab023aa9c0645d04120e
korenev <ledaynoy44@hmail.com>
2020-06-24 15:34:53 +0400

    chage state to store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import state, {stateAddPost, stateChangePostValue} from "./redux/state";
-import reRenderer from "./Renderer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {BrowserRouter} from "react-router-dom";
+import store from "./redux/state";
 
-reRenderer(state, stateAddPost, stateChangePostValue);
+
+const reRenderer = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                     stateAddPost={store.stateAddPost.bind(store)}
+                     stateChangePostValue={store.stateChangePostValue.bind(store)}
+                />
+            </BrowserRouter>
+        </React.StrictMode>, document.getElementById('root')
+    );
+}
+
+reRenderer(store.getState())
+
+store.subscriber(reRenderer);
+
+
+
+
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,82 +1,93 @@
-import reRenderer from "../Renderer";
+const store = {
+    _state: {
+        navBar: {
+            menu: [
+                {link: '/profile', name: 'Profile'},
+                {link: '/dialogs', name: 'Dialogs'},
+                {link: '/news', name: 'News'},
+                {link: '/bookmarks', name: 'Bookmarks'},
+            ],
+            friends: [
+                {id: 1, name: 'Misha', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
+                {id: 2, name: 'Yan', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
+                {id: 3, name: 'Artem', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
+            ],
+        },
+        profilePage: {
+            postsData: [
+                {id: 1, message: `Hi, world!`, likesCount: 2},
+                {id: 2, message: 'Bruh', likesCount: 1000},
+                {id: 3, message: 'Falling in Reverse are cool!', likesCount: -100},
+                {id: 3, message: 'Limp Bizkit', likesCount: 777},
+            ],
+            newPostText: 'it-kamasutra',
+        },
+        dialogsPage: {
+            dialogsData: [
+                {
+                    id: 1,
+                    name: 'Artemiy',
+                    img: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png'
+                },
+                {
+                    id: 2,
+                    name: 'Arina',
+                    img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Kseniya',
+                    img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpg'
+                },
+                {
+                    id: 4,
+                    name: 'Babushka',
+                    img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpgg'
+                },
+                {
+                    id: 5,
+                    name: 'Ded',
+                    img: 'https://image.flaticon.com/icons/svg/147/147144.svg'
+                },
+                {id: 13, name: 'Cat'},
+                {id: 7, name: 'Mishka'},
+                {id: 22, name: 'Malishka'},
+            ],
+            messageData: [
+                {id: 1, message: 'hello my friend'},
+                {id: 2, message: 'Bruh', me: true},
+                {id: 3, message: 'What are you doing?'},
+                {id: 5, message: 'You betrying my head, again...', me: true},
+                {id: 6, message: 'Khe Khe ;)'},
+            ],
+        },
+    },
 
-const state = {
-    navBar: {
-        menu: [
-            {link: '/profile', name: 'Profile'},
-            {link: '/dialogs', name: 'Dialogs'},
-            {link: '/news', name: 'News'},
-            {link: '/bookmarks', name: 'Bookmarks'},
-        ],
-        friends: [
-            {id: 1, name: 'Misha', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
-            {id: 2, name: 'Yan', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
-            {id: 3, name: 'Artem', img: 'http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png'},
-        ],
+    stateAddPost() {
+        const newPost = {
+            id: 5, message: this._state.profilePage.newPostText, likesCount: 0
+        };
+        this._state.profilePage.postsData.push(newPost);
+        this.re(this._state);
+        this.stateChangePostValue('');
     },
-    profilePage: {
-        postsData: [
-            {id: 1, message: `Hi, world!`, likesCount: 2},
-            {id: 2, message: 'Bruh', likesCount: 1000},
-            {id: 3, message: 'Falling in Reverse are cool!', likesCount: -100},
-            {id: 3, message: 'Limp Bizkit', likesCount: 777},
-        ],
-        newPostText: 'it-kamasutra',
+
+    stateChangePostValue(value) {
+        this._state.profilePage.newPostText = value;
+        this.re(this._state);
     },
-    dialogsPage: {
-        dialogsData: [
-            {
-                id: 1,
-                name: 'Artemiy',
-                img: 'https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png'
-            },
-            {
-                id: 2,
-                name: 'Arina',
-                img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpg'
-            },
-            {
-                id: 3,
-                name: 'Kseniya',
-                img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpg'
-            },
-            {
-                id: 4,
-                name: 'Babushka',
-                img: 'https://cdn5.vectorstock.com/i/thumb-large/61/44/avatar-business-woman-graphic-vector-9646144.jpgg'
-            },
-            {
-                id: 5,
-                name: 'Ded',
-                img: 'https://image.flaticon.com/icons/svg/147/147144.svg'
-            },
-            {id: 13, name: 'Cat'},
-            {id: 7, name: 'Mishka'},
-            {id: 22, name: 'Malishka'},
-        ],
-        messageData: [
-            {id: 1, message: 'hello my friend'},
-            {id: 2, message: 'Bruh', me: true},
-            {id: 3, message: 'What are you doing?'},
-            {id: 5, message: 'You betrying my head, again...', me: true},
-            {id: 6, message: 'Khe Khe ;)'},
-        ],
+
+    re() {
+        console.log('no observer subscribed')
     },
+
+    subscriber(observer) {
+        this.re = observer;
+    },
+
+    getState() {
+        return this._state;
+    }
 }
 
-export const stateAddPost = () => {
-
-    const newPost = {
-        id: 5, message: state.profilePage.newPostText, likesCount: 0
-    };
-    state.profilePage.postsData.push(newPost);
-    reRenderer(state, stateAddPost, stateChangePostValue);
-    stateChangePostValue('');
-}
-
-export const stateChangePostValue = (value) => {
-
-    state.profilePage.newPostText = value;
-    reRenderer(state, stateAddPost, stateChangePostValue);
-}
-export default state;
+export default store;
